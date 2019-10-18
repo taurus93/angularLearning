a776a0dd8c83a4fd128307f2078d30dfea2bdd6e
To Hoai Nam <namth@nara.com.vn>
2019-10-18 10:10:05 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angularLearning';
+
+  genericFunc<T>(argument: T): T[] {
+    const arrayOfT: T[] = [];    // Create empty array of type T.
+    arrayOfT.push(argument);   // Push, now arrayOfT = [argument].
+    return arrayOfT;
+  }
+
+  ngOnInit(): void {
+    const arrayFromString = this.genericFunc<string>('beep');
+    console.log(arrayFromString[0]);         // "beep"
+    console.log(typeof arrayFromString[0]);   // String
+
+    const arrayFromNumber = this.genericFunc(42);
+    console.log(arrayFromNumber[0]);         // 42
+    console.log(typeof arrayFromNumber[0]);   // number
+  }
+}
